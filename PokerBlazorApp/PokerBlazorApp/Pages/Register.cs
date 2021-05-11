c91f56a09d02652648bb47cdd308f8fca8e8a11e
jakub.sienkiewicz@nexus-ag.de <jakub.sienkiewicz@nexus-ag.de>
2021-05-11 08:02:36 +0200

    init register

---
+++ PokerBlazorApp/PokerBlazorApp/Pages/Register.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using PokerBlazorApp.Model;
+using PokerBlazorApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PokerBlazorApp.Pages
+{
+    public partial class Register
+    {
+        public UserCredentials UserCredentials { get; set; } = new UserCredentials();
+
+        [Inject]
+        public IRegisterService RegisterService { get; set; }
+
+
+        private async void HandleValidSubmit()
+        {
+
+        }
+    }
+}
--- PokerBlazorApp/PokerBlazorApp/Startup.cs
+++ PokerBlazorApp/PokerBlazorApp/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using PokerBlazorApp.Data;
+using PokerBlazorApp.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,6 +29,7 @@ namespace PokerBlazorApp
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<RegisterService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
